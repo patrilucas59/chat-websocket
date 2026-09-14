9c211c42e4cd42bf7016d1dc1d72d553526faa99
Lucas Patrício <patriciolucas059@gmail.com>
2026-09-14 19:18:35 -0300

    feat(server.js): WebSocketServer anexado para servidor http

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import http from 'node:http';
 
+import { WebSocketServer } from 'ws';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,16 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3333;
 const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (wss) => {
+  console.log('Novo cliente conectado via WebSocket');
+
+  wss.on('close', () => {
+    console.log('Cliente desconectado');
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
