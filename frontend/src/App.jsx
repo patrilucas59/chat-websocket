56e50ed41ce3bc310d844c20b73ebacc0e0499f6
Lucas Patrício <patriciolucas059@gmail.com>
2026-09-18 20:25:04 -0300

    chore(frontend): configuração do TailwindCSS no frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>Chat WebSocket</h1>
+    <div className="bg-black">
+      <h1 className='text-white text-3xl font-bold'>Chat WebSocket</h1>
     </div>
   );
 }
